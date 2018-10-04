e0dd211d2b41954e0adb5bded02992424423ef7d
Alessandro Arici <a.arici@techgap.it>
2018-10-04 17:21:58 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+      <div class="one-fourth">
+          <circular-percentage :size="size" :done="3" :undone="1" arc-color="#0000ff" bgCircleColor="#0000bb33" />
+      </div>
+      <div class="one-fourth">
+        <circular-percentage size="80" :done="60" :undone="30" arc-color="#ff0000" bgCircleColor="#ff000033" />
+      </div>
+      <div class="one-fourth">
+        <circular-percentage size="60" :done=done :undone=undone />
+      </div>
+      <div class="one-fourth">
+          <circular-percentage size="40" :done="90" :undone="10" arc-color="#fff000" bgCircleColor="#fff00033" />
+      </div>
+  </div>
+</template>
+
+<script>
+import CircularPercentage from './components/CircularPercentage.vue'
+
+export default {
+  name: 'app',
+  components: {
+      CircularPercentage
+  },
+    data() {
+        return {
+            size: 100,
+            done: 5,
+            undone: 20
+      }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+
+.one-fourth {
+    width: 25%;
+    float: left;
+}
+</style>
---
+++ src/components/CircularPercentage.vue
@@ -0,0 +1,114 @@
+<template>
+    <div>
+        <div class="flex-wrapper">
+            <svg :viewBox=viewBox class="circular-chart">
+                <path class="circle" :style=circleStroke :d=circlePath />
+                <path class="completed-arc" :style=arcStroke :stroke-dasharray=dashArray :d=circlePath />
+                <text :x=box/2 :y=(diameter/2+textSize/2)+1 class="percentage" :style=fontTextSize>{{percentage}}%</text>
+            </svg>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'CircularPercentage',
+        props: {
+            size: {
+                size: Number,
+                default: 100
+            },
+            done: {
+                type: Number,
+                required: true
+            },
+            undone: {
+                type: Number,
+                required: true
+            },
+            arcColor: {
+                type: String,
+                default: '#00c71b'
+            },
+            bgCircleColor: {
+                type: String,
+                default: '#00cc1b33',
+            }
+        },
+        computed: {
+            percentage() {
+                return parseFloat(100 / (this.done + this.undone) * this.done).toFixed(1);
+            },
+            dashArray() {
+                let perc = this.size / 100 * this.percentage;
+                return perc + ', ' + this.size;
+            },
+            radius() {
+                return this.size / (2 * 3.14159);
+            },
+            diameter() {
+                return this.radius * 2;
+            },
+            box() {
+                return 36;
+            },
+            viewBox() {
+                return '0 0 ' + this.box + ' ' + this.box;
+            },
+            circlePath() {
+                return 'M' + (this.box / 2) + ' 2.08' /*([this.box - this.diameter] / 2)*/
+                    + ' a ' + this.radius + ' ' + this.radius + ' 0 0 1 0 ' + this.diameter
+                    + ' a ' + this.radius + ' ' + this.radius + ' 0 0 1 0 -' + this.diameter;
+
+            },
+            textSize() {
+                return this.size / 11;
+            },
+            fontTextSize() {
+                return 'font-size: ' + this.textSize + 'px';
+            },
+            arcStroke() {
+                return 'stroke: ' + this.arcColor;
+            },
+            circleStroke() {
+                return 'stroke: ' + this.bgCircleColor;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .flex-wrapper {
+        display: flex;
+        flex-flow: row nowrap;
+    }
+
+    .circular-chart {
+        max-width: 90%;
+        max-height: 200px;
+        margin: 5px auto;
+    }
+
+    .circle {
+        fill: none;
+        stroke-width: 2.0;
+    }
+
+    .completed-arc {
+        fill: none;
+        stroke-width: 1.3;
+        stroke-linecap: round;
+        animation: progress 2s ease-out forwards;
+    }
+
+    @keyframes progress {
+        0% {
+            stroke-dasharray: 0 100;
+        }
+    }
+
+    .percentage {
+        fill: #666164;
+        text-anchor: middle;
+    }
+</style>
